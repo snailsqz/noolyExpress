ef0f001b58c0d37f84e6df2c33bae43c3ea0e3db
snails <snailsqz@gmail.com>
2024-01-15 22:46:50 +0700

    fin

--- shufflingNum.js
+++ shufflingNum.js
@@ -1,3 +1,22 @@
-let shufflingNum = () =>{
-    
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
+
+let shufflingNum = (e) => {
+  let trop = [];
+  let nums;
+
+  if (typeof e == "string") nums = e.split("");
+  else if (typeof e == "object") nums = [...e];
+
+  while (nums.length != 0) {
+    let randomNum = getRandomInt(nums.length);
+    trop.push(nums[randomNum]);
+    nums.splice(nums.indexOf(nums[randomNum]), 1);
+  }
+
+  return trop;
+};
+
+console.log(shufflingNum([2, 1, 6, 7, 3, 4]));
+console.log(shufflingNum("230959"));
