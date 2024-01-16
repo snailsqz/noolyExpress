8548ad586e9a127695d6c1916a93a00527f892a0
snails <snailsqz@gmail.com>
2024-01-17 00:10:44 +0700

    fin

--- findDuplicate.js
+++ findDuplicate.js
@@ -2,10 +2,7 @@ function findDupe(nums) {
   let foundNum = [];
   for (let i = 0; i < nums.length; i++) {
     for (let j = i; j < nums.length; j++) {
-      if (nums[i] == nums[j + 1]) {
-        foundNum.push(nums[i]);
-        console.log("hi");
-      }
+      if (nums[i] == nums[j + 1]) foundNum.push(nums[i]);
     }
   }
   return foundNum;
