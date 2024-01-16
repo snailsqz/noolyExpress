d10d66552c64ffa99af17403839d4dd904a567b0
snails <snailsqz@gmail.com>
2024-01-17 00:10:00 +0700

    fin

--- 10question2knowme/removeDupe.js
+++ 10question2knowme/removeDupe.js
@@ -2,13 +2,10 @@ function removeDups(arr) {
   let op = [];
   for (let i = 0; i < arr.length; i++)
     for (let j = i + 1; j < arr.length; j++) if (arr[i] == arr[j]) op.push(j);
-
   op.sort(function (a, b) {
     return b - a;
   });
-
   for (let i = 0; i < op.length; i++) arr.splice(op[i]);
-
   return arr;
 }
 
